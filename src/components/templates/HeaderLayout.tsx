b8206ef360fe705d384ae7ab47f7f7d8afc92eaa
hiro <hmaeyama0510@gmail.com>
2021-06-02 16:59:28 +0900

    render the headelayout component to /home compoenents

--- src/components/templates/HeaderLayout.tsx
+++ src/components/templates/HeaderLayout.tsx
@@ -3,15 +3,15 @@ import { memo, ReactNode, VFC } from "react";
 import { Header } from "../organisms/layout/Header";
 
 type Props = {
-  chidlren: ReactNode;
+  children: ReactNode;
 };
 
-export const HeadeLayout: VFC<Props> = memo((props) => {
-  const {children} = props;
+export const HeaderLayout: VFC<Props> = memo((props) => {
+  const { children } = props;
   return (
     <>
       <Header />
-      { children }
+      {children}
     </>
-  )
+  );
 });
