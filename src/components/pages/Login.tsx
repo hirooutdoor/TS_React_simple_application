db46d7c014e9fa30d2707b60f4fe229352fcf513
hiro <hmaeyama0510@gmail.com>
2021-06-04 04:44:28 +0900

    Create boilerplate of Login page

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,14 @@
+import { Box, Button, Flex, Heading, Input } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const Login: VFC = memo(() => {
-  return <p>Login Page</p>;
+  return (
+    <Flex>
+      <Box>
+        <Heading as="h1">User admin app</Heading>
+        <Input placeholder="user ID" />
+        <Button>Login</Button>
+      </Box>
+    </Flex>
+  );
 });
