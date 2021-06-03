32749880442f3e58735a01d640da1985ba0ec434
hiro <hmaeyama0510@gmail.com>
2021-06-04 04:36:59 +0900

    Add routing into ManuDrawer

--- src/components/molecules/MenuDrawer.tsx
+++ src/components/molecules/MenuDrawer.tsx
@@ -10,19 +10,34 @@ import { memo, VFC } from "react";
 type Props = {
   onClose: () => void;
   isOpen: boolean;
+  onClickHome: () => void;
+  onClickSetting: () => void;
+  onClickUserManagement: () => void;
 };
 
 export const MenuDrawer: VFC<Props> = memo((props) => {
-  const { onClose, isOpen } = props;
+  const {
+    onClose,
+    isOpen,
+    onClickHome,
+    onClickSetting,
+    onClickUserManagement
+  } = props;
 
   return (
     <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
       <DrawerOverlay>
         <DrawerContent>
           <DrawerBody p={0} bg="gray.100">
-            <Button w="100%">TOP</Button>
-            <Button w="100%">Users</Button>
-            <Button w="100%">Settings</Button>
+            <Button w="100%" onClick={onClickHome}>
+              TOP
+            </Button>
+            <Button w="100%" onClick={onClickUserManagement}>
+              Users
+            </Button>
+            <Button w="100%" onClick={onClickSetting}>
+              Settings
+            </Button>
           </DrawerBody>
         </DrawerContent>
       </DrawerOverlay>
