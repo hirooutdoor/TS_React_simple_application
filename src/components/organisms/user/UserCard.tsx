0dbf19ef83abaa66ff0b7aa0b9a8dcfd0656c19f
hiro <hmaeyama0510@gmail.com>
2021-06-04 09:59:43 +0900

    create UserCard component

---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,32 @@
+import { Box, Image, Stack, Text } from "@chakra-ui/react";
+import { memo, VFC } from "react";
+
+type Props = {
+  imageUrl: string;
+  userName: string;
+  fullName: string;
+};
+
+export const UserCard: VFC<Props> = memo((props) => {
+  const { imageUrl, userName, fullName } = props;
+
+  return (
+    <Box w="260px" h="260px" bg="white" borderRadius="10px" shadow="md" p={4}>
+      <Stack textAlign="center">
+        <Image
+          boxSize="160px"
+          borderRadius="full"
+          src={imageUrl}
+          m="auto"
+          alt={userName}
+        />
+        <Text fontSize="lg" fontWeight="bold">
+          {userName}
+        </Text>
+        <Text fontSize="sm" color="gray">
+          {fullName}
+        </Text>
+      </Stack>
+    </Box>
+  );
+});
--- src/components/pages/UserManageMent.tsx
+++ src/components/pages/UserManageMent.tsx
@@ -1,35 +1,18 @@
-import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
   return (
     <Wrap p={{ base: 4, md: 10 }}>
       {[...Array(10)].map(() => (
         <WrapItem _hover={{ cursor: "pointer", opacity: 0.8 }}>
-          <Box
-            w="260px"
-            h="260px"
-            bg="white"
-            borderRadius="10px"
-            shadow="md"
-            p={4}
-          >
-            <Stack textAlign="center">
-              <Image
-                boxSize="160px"
-                borderRadius="full"
-                src="https://source.unsplash.com/random"
-                m="auto"
-                alt="profile image."
-              />
-              <Text fontSize="lg" fontWeight="bold">
-                Armin
-              </Text>
-              <Text fontSize="sm" color="gray">
-                Armin Hetlert
-              </Text>
-            </Stack>
-          </Box>
+          <UserCard
+            imageUrl="https://source.unsplash.com/random"
+            userName="Armin"
+            fullName="Armin Hertlert"
+          />
         </WrapItem>
       ))}
     </Wrap>
