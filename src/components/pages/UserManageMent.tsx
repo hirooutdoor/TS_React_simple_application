db5efb036e7ee762c6ac7cf59ae71cf2e31c6201
hiro <hmaeyama0510@gmail.com>
2021-06-04 09:05:14 +0900

    add style to boxes

--- src/components/pages/UserManageMent.tsx
+++ src/components/pages/UserManageMent.tsx
@@ -1,18 +1,35 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
+import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const UserManagement: VFC = memo(() => {
   return (
-    <Wrap spacing="10px">
+    <Wrap p={{ base: 4, md: 10 }}>
       {[...Array(10)].map(() => (
-        <WrapItem>
-          <div
-            style={{
-              width: "100px",
-              height: "100px",
-              backgroundColor: "teal"
-            }}
-          />
+        <WrapItem _hover={{ cursor: "pointer", opacity: 0.8 }}>
+          <Box
+            w="260px"
+            h="260px"
+            bg="white"
+            borderRadius="10px"
+            shadow="md"
+            p={4}
+          >
+            <Stack textAlign="center">
+              <Image
+                boxSize="160px"
+                borderRadius="full"
+                src="https://source.unsplash.com/random"
+                m="auto"
+                alt="profile image."
+              />
+              <Text fontSize="lg" fontWeight="bold">
+                Armin
+              </Text>
+              <Text fontSize="sm" color="gray">
+                Armin Hetlert
+              </Text>
+            </Stack>
+          </Box>
         </WrapItem>
       ))}
     </Wrap>
